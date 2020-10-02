4bc9c6f8f17ffcab8bb45dfb12a08e0432aa32ba
Manh-Hoang-07 <43363434+Manh-Hoang-07@users.noreply.github.com>
2020-10-02 15:01:53 +0700

    Update HeaderComponent.js

--- English/components/HeaderComponent.js
+++ English/components/HeaderComponent.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 const Header = (props) => {
     const { headerStyle, bgHeader } = styles;
   
     return (
       <View style = {bgHeader}>
-        <Text style = { headerStyle }>{props.headerText}</Text>
+        <Text style = { headerStyle }><Image style={styles.imageButtonStyle} source={require('./menu.png')}></Image>              {props.headerText}</Text>
       </View>
     );
   };
@@ -14,15 +14,17 @@ const Header = (props) => {
 const styles = {
     bgHeader: {
       backgroundColor: '#0288D1',
-      justifyContent:'center',
-      alignItems:'center',
+      
     },
     headerStyle: {
       fontSize: 25,
-      textAlign: 'center',
+      
       margin: 10,
       color: '#fff',
     },
+    imageStyle:{
+      width: 30, height: 30
+    }
   };
 
 //export component để dùng ở 1 nơi khác
